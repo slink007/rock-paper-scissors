7fb0d66e30a1caa8ea1ef56652b7cd03ee345a81
Steve <stephen.link0@gmail.com>
2020-01-25 19:00:36 -0500

    added description

---
+++ rps.py
@@ -0,0 +1,9 @@
+# Assignment from Section 9 of Modern Python 3 Bootcamp
+# A basic rock, paper, scissors game
+# How basic is it?  We ignore the fact that when Player 1 enters a choice
+#    Player 2 can see that and totally cheat.
+#
+# paper > rock
+# scissors > paper
+# rock > scissors
+
